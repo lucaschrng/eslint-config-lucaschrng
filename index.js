66491ed8aed4dc41b400c595f083592fdc51cc08
Lucas Charoing <charoing.lucas@gmail.com>
2024-11-24 16:50:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      "indent": ["error", 2],
+      "no-trailing-spaces": "error",
+      "no-multiple-empty-lines": "error",
+      "no-multi-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "eol-last": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+    }
+  }
+]
